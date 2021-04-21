e939b6f4542ca20faddf4f2faf572829f6aed3e9
Poonsak Aphidetmongkhon <poonsak_lds@hotmail.com>
2021-04-21 07:47:45 +0700

    update fork & vscode

--- setup-follow.sh
+++ setup-follow.sh
@@ -4,10 +4,12 @@ xcode-select --install
 brew update
 brew install --cask iterm2
 brew install git
+# git tools fork or sourcetree. (up to you)
+brew install --cask fork
 brew install --cask sourcetree
 # install nvm/node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 nvm install stable
-brew install --cask vscodium
+brew install --cask visual-studio-code
 brew install --cask postman
 brew install --cask proxyman
